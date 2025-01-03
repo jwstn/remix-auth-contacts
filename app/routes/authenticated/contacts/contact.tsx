cf4d2475710e8fc659e24d006e64d34c8e62989c
DevNewbieJW <julian.weusten@fondof.de>
2025-01-03 02:27:12 +0100

    delete user

--- app/routes/authenticated/contacts/contact.tsx
+++ app/routes/authenticated/contacts/contact.tsx
@@ -1,9 +1,39 @@
+import { TypographyH1, TypographyP } from "@/components/typography";
+import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import { contactsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { Trash2Icon } from "lucide-react";
+import { Form, redirect } from "react-router";
 import type { Route } from "./+types/contact";
 
 export async function loader({ params }: Route.ActionArgs) {
-  return params;
+  const contact = await db
+    .select()
+    .from(contactsTable)
+    .where(eq(contactsTable.id, Number.parseInt(params.contactId)));
+  return contact[0];
 }
 
 export default function Contact({ loaderData }: Route.ComponentProps) {
-  return <div>{JSON.stringify(loaderData, null, 2)}</div>;
+  return (
+    <div className="flex w-full max-w-screen-lg">
+      <img alt="" src="" className="min-w-96 min-h-96" />
+      <div className="space-y-0 px-2">
+        <TypographyH1>{loaderData.name}</TypographyH1>
+        <TypographyP>{loaderData.email}</TypographyP>
+      </div>
+
+      <Form method="post">
+        <Button type="submit" variant="destructive" size="icon">
+          <Trash2Icon />
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ params }: Route.ActionArgs) {
+  await db.delete(contactsTable).where(eq(contactsTable.id, Number.parseInt(params.contactId)));
+  return redirect("/");
 }
