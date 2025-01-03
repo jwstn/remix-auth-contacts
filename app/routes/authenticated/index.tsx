2c2bf216315a5675c44077dabf0a0cb1bd1c1c50
DevNewbieJW <julian.weusten@fondof.de>
2025-01-03 01:32:16 +0100

    add auth layout

---
+++ app/routes/authenticated/index.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/index";
+
+export function meta() {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export function loader({ request, context, params }: Route.LoaderArgs) {
+  // console.log(request, context, params);
+  return {};
+}
+
+export default function Index({ loaderData, matches, params, actionData }: Route.ComponentProps) {
+  // console.log(loaderData, matches, params, actionData);
+  return <div className="text-red-500">home</div>;
+}
